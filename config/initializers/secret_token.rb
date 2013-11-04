67f1a85e4ec086cbaccc6ad1fd891f5480c7fe72
Andrew Khor <akhor93@gmail.com>
2013-11-03 16:21:54 -0800

    New App. Just initialized RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiveMeTime::Application.config.secret_token = 'c187f1cb1728c0f9b9f636899ed42d7babe6fbf964d5aa0d23203a23fd06d4cc1396793a60a86041a9c1b1f4b3d99871bf9983a0b43df480860201e14a82545e'
