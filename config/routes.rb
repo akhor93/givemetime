9df4a8082710d1a4b169ab39d177a91768678120
Andrew Khor <akhor93@gmail.com>
2013-11-17 15:57:56 -0800

    When logged in and you go to home page, you get redirected to the user#show page

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,10 @@ GiveMeTime::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  constraints lambda { |req| !req.session[:user_id].blank? } do
+    root :to => "users#show", :as => "calendar"
+  end
+
   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
