a5710128d5e00570d8459c9b39dc25d470e298ea
Andrew Khor <akhor93@gmail.com>
2013-12-17 03:05:56 -0600

    Implemented current time bar, update position every minute, and the scroll top(rough, could be refined but looks good at 3:05am).

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -23,5 +23,19 @@ $(document).ready(function() {
 		$('#todo_duration').val($.trim($this.text()));
 	});
 
-	
+	var currentdate = new Date();
+	var block_duration = 5;
+	var block_height = 25;
+	var num_blocks = (currentdate.getHours() * 60 + currentdate.getMinutes()) / block_duration;
+	var top_offset = num_blocks * block_height;
+	$('#current_time_bar').css('top',top_offset);
+
+	$('#current_time_bar').css('top',"-=5");
+	(function moveCurrentTimeBar() {
+		$('#current_time_bar').css('top',"+=5");
+		setTimeout(moveCurrentTimeBar, 60000);
+	})();
+
+	var current_time_bar_scroll_top = parseInt($('#current_time_bar').css('top'), 10);
+	$('#calendar').scrollTop(current_time_bar_scroll_top);
 });
