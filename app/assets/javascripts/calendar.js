8fab1bf2d231b6d489de4c7f8dc3ea40b92448f0
Andrew Khor <akhor93@gmail.com>
2013-12-20 01:05:13 -0600

    Did UI Overhaul. Moved stuff and cleaned up code

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,74 +1,23 @@
 $(document).ready(function() {
-	$('.event_duration_button').click(function() {
-		//Clear existing active
-		$('.event_duration_button').removeClass('active');
-		//Add 'Active' class to button 
-		var $this = $(this);
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		//Assign Value to hidden field elem
-		$('#event_duration').val($.trim($this.text()));
+	$('#logout_button').click(function() {
+		$('#logout_link').click();
 	});
 
-	$('.todo_duration_button').click(function() {
-		//Clear existing active
-		$('.todo_duration_button').removeClass('active');
-		//Add 'Active' class to button 
-		var $this = $(this);
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		//Assign Value to hidden field elem
-		$('#todo_duration').val($.trim($this.text()));
-	});
+	$('.event_duration_button').click(update_duration);
+	$('.todo_duration_button').click(update_duration);
 
 	var events_list = new Array();
 	var future_events = new Array(); //Includes currently happening events
 	var alert_queue = new Array();
-	function Cal_Event(title, duration, start) {
-		var start_moment = moment(start, "YYYY-MM-DD HH:mm:ss Z");
-		var end_moment = moment(start_moment);
-		end_moment.add("seconds", parseInt(duration,10));
-
-		this.title = title;
-		this.duration = duration;
-		this.start = start_moment;
-		this.end = end_moment;
-		//Decompose following three lines later
-		this.inPast = moment() > this.end;
-		this.inFuture = moment() < this.start;
-		this.isHappening = !this.inPast && !this.inFuture
-		//console.log(this);
-		// console.log(start_moment.hour());
-		// console.log(end_moment.hour());
-		// var now = moment();
-		// this.isPast = now.diff(start + duration)
-		// console.log(start);
-		
-	}
+	
 
 	$('#event_container').on('append_event', function( event, title, duration, start) {
 		var event = new Cal_Event(title, duration, start);
 		events_list.push(event);
-		if(!event.isPast) future_events.push(event);
-		//earliest events are at end of array. Allows for pop to work well
+		if(!event.inPast) future_events.push(event);
+		//earliest events are at end of array. This is to use pop over shift
 		future_events.sort(sort_events_reverse_chrono_start);
-		// events_list.sort(sort_events_chrono_start);
-		// console.log(events_list);
 	});
-
-	function sort_events_chrono_start(a,b) {
-		// console.log(a.title + ' ' + b.title);
-		var val = (a.start - b.start);
-		// console.log(val);
-		return val;
-	}
-
-	function sort_events_reverse_chrono_start(a,b) {
-		return (b.start - a.start);
-	}
-
 
 	var now = moment();
 	var now_end = moment(now).endOf('minute');
@@ -81,37 +30,36 @@ $(document).ready(function() {
 	
 	var first = true;
 	var millisTillNextMin = now_end - now;
-	console.log(millisTillNextMin);
 	setTimeout(function moveCurrentTimeBar() {
 		if(first) {
 			$('#current_time_bar').css('top',"-=5");
 			first = false;
 		}
 
-		console.log(future_events.length);
+		// console.log(future_events.length);
 		if(future_events.length > 0) {
-			console.log('1');
+			// console.log('1');
 			//need to guarantee the end of the first event is before the start of the second
-			console.log(moment());
-			console.log(future_events[future_events.length-1].end);
+			// console.log(moment());
+			// console.log(future_events[future_events.length-1].end);
 			if(moment() > future_events[future_events.length-1].end) {
-				console.log('2');
+				// console.log('2');
 				if(future_events.length > 1) {
-					console.log('4*');
+					// console.log('4*');
 					//Check if space between next event is more than 5 minutes. Need to account for time
 					//user takes to respond and add an event. A strict 5 minutes wont work.
 					if(future_events[future_events.length-2].start > future_events[future_events.length-1].end.add('minutes',5)) {
-						console.log('5');
+						// console.log('5');
 						//Enough space, show alert
 						alert("You have some free time. Why don't you complete some activities or to-dos");
 					}
 					else {
-						console.log('6');
+						// console.log('6');
 						//Not Enough space between events. Keep for readability
 					}
 				}
 				else {
-					console.log('3');
+					// console.log('3');
 					//Only event, so send alert
 					alert("You have some free time. Why don't you complete some activities or to-dos");
 				}
@@ -134,11 +82,58 @@ $(document).ready(function() {
 	$('#calendar').scrollTop(current_time_bar_scroll_top);
 
 	$('#calendar').perfectScrollbar({
-		wheelSpeed: 100,
+		wheelSpeed: 150,
 		minScrollbarLength: 200
 	});
 
 
 	$('#load_google_events').click();
 
+	$('.collapse').on('show.bs.collapse hide.bs.collapse', function() {
+		var i = $(this).parent().find('.glyphicon');
+		i.toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
+	});
+
 });
+
+//Objects
+function Cal_Event(title, duration, start) {
+		var start_moment = moment(start, "YYYY-MM-DD HH:mm:ss Z");
+		var end_moment = moment(start_moment);
+		end_moment.add("seconds", parseInt(duration,10));
+
+		this.title = title;
+		this.duration = duration;
+		this.start = start_moment;
+		this.end = end_moment;
+		//Decompose following three lines later
+		this.inPast = moment() > this.end;
+		this.inFuture = moment() < this.start;
+		this.isHappening = !this.inPast && !this.inFuture
+	}
+
+//Functions
+function logout() {
+	$('#logout_link').click();
+}
+
+function update_duration() {
+	var $this = $(this);
+	//Clear existing active
+	$('.' + $this.data('category') + '_duration_button').removeClass('active');
+	//Add 'Active' class to button 
+	if (!$this.hasClass('active')) {
+		$this.addClass('active');
+	}
+	//Assign Value to hidden field elem
+	$('#' + $this.data('category') + '_duration').val($.trim($this.text()));
+}
+
+//Utility
+function sort_events_chrono_start(a,b) {
+	return (a.start - b.start);
+}
+
+function sort_events_reverse_chrono_start(a,b) {
+	return (b.start - a.start);
+}
